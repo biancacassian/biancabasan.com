aab595a39b33839c3b8f51bf0596a4c04401d022
taleldayekh <dev@talel.io>
2022-04-30 19:02:22 +0200

    Adding images to double col content

--- components/ContentView/DoubleColContentView.tsx
+++ components/ContentView/DoubleColContentView.tsx
@@ -1,22 +1,31 @@
-import { ContentViewProps } from './interfaces';
 import { Fragment } from 'react';
+import { ContentViewProps } from './interfaces';
 import doubleColStyles from './styles/double-col-styles.module.css';
 
 const DoubleColContentView = ({ contents }: ContentViewProps): JSX.Element => {
   return (
     <div className={doubleColStyles['double-col']}>
-      {contents.map((content, index) => (
-        <Fragment key={index}>
-          {content.header}
-          <div className={doubleColStyles['double-col__paragraphs']}>
-            {content.body.map((paragraph, index) => (
+      {contents.map((content, index) => {
+        const images = content.images;
+
+        return (
+          <Fragment key={index}>
+            {content.header}
+            <div className={doubleColStyles['double-col__paragraphs']}>
+              {content.body.map((paragraph, index) => (
+                <Fragment key={index}>
+                    {paragraph}
+                </Fragment>
+              ))}
+            </div>
+            {images && images.map((image, index) => (
               <Fragment key={index}>
-                  {paragraph}
+                {image}
               </Fragment>
             ))}
-          </div>
-        </Fragment>
-      ))}
+          </Fragment>
+        )
+      })}
     </div>
   )
 }
