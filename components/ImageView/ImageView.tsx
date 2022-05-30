a87dd9f234779f002903822d75a0f790a8f842d2
Bianca Cassian <106512500+biancacassian@users.noreply.github.com>
2022-05-30 10:30:01 +0200

    Update ImageView.tsx

--- components/ImageView/ImageView.tsx
+++ components/ImageView/ImageView.tsx
@@ -2,10 +2,14 @@ import Image from 'next/image';
 import { ImageViewProps } from './interfaces';
 import imageStyles from './styles/image.styles.module.css';
 
+const customLoader = ({ src }) => {
+  return src
+}
+
 const ImageView = ({className, image, imageDescription, imageDescriptionUrl, centerImageDescription, imageDisplay, imageAltText}: ImageViewProps): JSX.Element => {
   return (
     <div className={`${imageStyles[`image-styles__${imageDisplay}`]} ${className}`}>
-      <Image src={image} alt={imageAltText}/>
+      <Image loader={customLoader} src={image} alt={imageAltText}/>
       { imageDescription && !imageDescriptionUrl &&  <p className={`${imageStyles['image-styles__image-description']} ${centerImageDescription && imageStyles['image-styles__image-description__centered']}`}>{imageDescription}</p>}
       { imageDescription && imageDescriptionUrl && <a href={imageDescriptionUrl} rel="noopener noreferrer" target="_blank"><p className={`${imageStyles['image-styles__image-description']} ${centerImageDescription && imageStyles['image-styles__image-description__centered']}`}>{imageDescription}</p></a> }
     </div>
