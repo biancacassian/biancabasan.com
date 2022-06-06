f6228cd03f770377353ae9273b9fceb417dc61a0
Bianca Cassian <106512500+biancacassian@users.noreply.github.com>
2022-06-06 21:05:20 +0200

    Update LessonsLearned.tsx

--- components/LessonsLearned/LessonsLearned.tsx
+++ components/LessonsLearned/LessonsLearned.tsx
@@ -3,7 +3,7 @@ import ImageView from '../ImageView/ImageView'
 import contentStyles from '../ContentView/styles/content.styles.module.css';
 import feedback from '../../public/assets/images/content/lessons-learned/lessons-learned-feedback.svg';
 import lessonsLearnedEnd from '../../public/assets/images/content/lessons-learned/lessons-learned-end.png';
-import lessonsLearnedStyles from './lessons-learned.styles.module.css';
+import lessonsLearnedStyles from './styles/lessons-learned.styles.module.css';
 
 const LessonsLearned = (): JSX.Element => {
   return (
@@ -11,8 +11,11 @@ const LessonsLearned = (): JSX.Element => {
       <h1 className={contentStyles['content__double-col-left']}>Even though this is a course project I reached out to a developer and asked for feedback</h1>
       <div className={contentStyles['content__double-col-right']}>
         <p>Working closely with a developer will be a crucial part of implementing the app.</p>
-        <p>I was curious to <b>learn and receive feedback on what is achievable and what will be easy to implement for this version of MVP, what are some technical considerations that I need to take into account?</b></p>
+        <p>I was curious to receive feedback on <b>how a developer wold perceive having my designs handed over.</b></p>
         <p>Here is his response:</p>
+        <div className={lessonsLearnedStyles['lessons-learned__quote-box']}>
+          <p><b>"Bianca's mockups leaves few questions unanswered. All elements are clearly laid out and the prototype shows the expected behavior of each screen. For a developer it is just a matter of picking up the design and only worry about how to implement it in code!"</b> - Talel, Software Developer</p>
+        </div>
         <p>Throughout the course, I appreciated the constructive feedback I received from my colleagues that helped me make better design decisions and think critically.</p>
       </div>
       <ImageView imageDescription='Feedback received from colleagues enrolled in the same course' image={feedback} imageAltText="Lessons Learned" imageDisplay={ImageDisplay.right}/>
