04683c502052df1fab35dd10c1a3d7ffc6d8ced2
Bianca Cassian <106512500+biancacassian@users.noreply.github.com>
2022-06-14 21:26:14 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ const Index = (): JSX.Element => {
       <div className={indexStyles['index__content']}>
         <ImageView className={indexStyles['index__content__image']} image={bianca} imageAltText="Bianca Name"/>
         <div className={indexStyles['index__content__welcome-message']}>
-          <p>Hi there! I'm a self taught, growth oriented UX/UI designer based in Berlin.</p>
+          <p>Hi there! I'm a self-taught, growth oriented UX/UI designer based in Berlin.</p>
           <p>As I spend my time on this planet, with the help of design and technology, I hope to improve people's lives and help them do things more enjoyable.</p>
         </div>
         <h1 className={indexStyles['index__content__case-studies-title']}>Case Studies</h1>
