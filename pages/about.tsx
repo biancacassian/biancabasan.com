7a20d672943c16c06fc5143ff3e8d073161bf2b2
taleldayekh <dev@talel.io>
2022-11-26 21:33:30 +0100

    Change wording

--- pages/about.tsx
+++ pages/about.tsx
@@ -10,38 +10,38 @@ const About = (): JSX.Element => {
     <>
       <div className={aboutStyles.about}>
         <div className={aboutStyles['about__wrapper']}>
-          <Navigation className={aboutStyles['about__navigation']}/>
+          <Navigation className={aboutStyles['about__navigation']} />
           <ContentView>
-            <ImageView className={aboutStyles['about__image-large']} image={biancaLarge} imageAltText="Bianca Large"/>
-            <ImageView className={aboutStyles['about__image-small']} image={biancaSmall} imageAltText="Bianca Small"/>
+            <ImageView className={aboutStyles['about__image-large']} image={biancaLarge} imageAltText="Bianca Large" />
+            <ImageView className={aboutStyles['about__image-small']} image={biancaSmall} imageAltText="Bianca Small" />
             <div className={aboutStyles['about__col-right']}>
               <p>For me design is a way to connect with the world and be a bridge between what we understand about the human being, its capabilities and technology. </p>
-              <p>What excites me about product design is that it lies at the intersection of cognitive science, human behavior, design and engineering.</p>
+              <p>What excites me about product design is that it lies at the intersection of human behavior, design and engineering.</p>
               <p>What motivates me about design is its collaborative and iterative approach to solving problems, as well as helping others (and myself) to do things more efficiently and become better every day.</p>
-              <p>Personally, I enjoy the way our emotions and memories are linked with specific technologies. I still remember my first ipod, it was green, and I couldn’t imagine that I had all my playlist in one place, always with me. Maybe you had one too, what color was yours? </p>
+              <p>Personally, I enjoy the way our emotions and memories are linked with specific technologies. I still remember my first iPod, it was green, and I couldn’t imagine that I had all my playlist in one place, always with me. Maybe you had one too, what color was yours? </p>
             </div>
-            <div className={aboutStyles['about__border']}/>
+            <div className={aboutStyles['about__border']} />
             <h1 className={aboutStyles['about__col-left']}>What I value</h1>
             <div className={aboutStyles['about__col-right']}>
               <p>Stay Humble</p>
               <p>Hard work and being able to help others</p>
               <p>As a way of improving myself and helping others to improve, I enjoy giving and asking for feedback. I would love in the near future to mentor.</p>
             </div>
-            <div className={aboutStyles['about__border']}/>
+            <div className={aboutStyles['about__border']} />
             <h1 className={aboutStyles['about__col-left']}>Learning</h1>
             <div className={aboutStyles['about__col-right']}>
               <p>Always be learning and working hard on cultivating a growth mindset</p>
               <p>Storytelling</p>
               <p>Practicing Fundamentals of design</p>
             </div>
-            <div className={aboutStyles['about__border']}/>
+            <div className={aboutStyles['about__border']} />
             <h1 className={aboutStyles['about__col-left']}>I'm keeping an eye on this digital product</h1>
             <div className={aboutStyles['about__col-right']}>
               <p>A product that I recommend to my friends is <b>OKU</b>, a social network for reading lovers.  I love the clean and the simplicity of the interface and that I can have a say and vote for upcoming features to be implemented.</p>
               <p><a className={aboutStyles["about__link"]} href="https://oku.club/user/BiancaBasan" rel="noopener noreferrer" target="_blank">Here is my profile</a>, let's share books!</p>
             </div>
 
-            <div className={aboutStyles['about__border']}/>
+            <div className={aboutStyles['about__border']} />
             <h1 className={aboutStyles['about__col-left']}>I'm glad you stopped by!</h1>
             <div className={aboutStyles['about__col-right']}>
               <p>We can talk about anything design, inclusion and psychology in design, personal development or review each others work.</p>
