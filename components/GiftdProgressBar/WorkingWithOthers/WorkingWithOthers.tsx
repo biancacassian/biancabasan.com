6e76bdb1988ff988d214c44b5aec3b775a877c5b
taleldayekh <dev@talel.io>
2023-03-29 21:25:30 +0200

    More changes

--- components/GiftdProgressBar/WorkingWithOthers/WorkingWithOthers.tsx
+++ components/GiftdProgressBar/WorkingWithOthers/WorkingWithOthers.tsx
@@ -7,11 +7,11 @@ import ideation from '../../../public/assets/images/giftd-progress-bar/ideation.
 const WorkingWithOthers = (): JSX.Element => {
     return (
         <>
-            <h1 className={contentStyles['content__double-col-left']}>"How can we emotionally connect users to their CO2e savings through the user interface?"</h1>
+            <h1 className={contentStyles['content__double-col-left']}>How can we emotionally connect users to their CO2e savings through the user interface?</h1>
             <div className={`${contentStyles['content__double-col-right']} ${empathizeStyles['empathize__col']}`}>
                 <p>Ideation is one of my favorite parts as <b>I really enjoy brainstorming with my colleagues towards the same goal, it’s also very inspiring to hear other people’s thoughts and building on each others ideas.</b></p>
-                <p> One of the questions that drove my designs during ideation was: how through the user interface to emotionally connect users to their CO2e impact?</p>
-                <p>During the brainstorm session we had an “aha” moment and thought to use metaphors and numbers to show the progress users have as they invite friends and gift items.</p>
+                <p>One of the questions that drove my designs during ideation was: how through the user interface to emotionally connect users to their CO2e impact?</p>
+                <p>During the ideation session we had an “aha” moment and thought to use metaphors and numbers to show the progress users have as they invite friends and gift items.</p>
             </div>
             <ImageView className={empathizeStyles['ideate']} image={ideation} imageAltText="Filters" imageDisplay={ImageDisplay.full} imageDescription={"Design explorations"} />
         </>
